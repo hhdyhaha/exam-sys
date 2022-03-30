ec56bb04330705bdd54a9a297648057fa88b061e
dgzz <1164549585@qq.com>
2022-03-30 19:31:16 +0800

    vue axios 数据（data）赋值问题

--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>我是主页</div>
+  <div>
+    我是主页
+  </div>
 </template>
 
 <script>
--- src/views/onlineexam/OlineExam.vue
+++ src/views/onlineexam/OlineExam.vue
@@ -1,18 +1,39 @@
 <template>
-  <div>在线考试</div>
+  <div>
+    <common-table  :tableData="tableData" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name:'onlineexam',
-        data() {
-          return {
-            
-          }
-        },
-    }
+import CommonTable from "../../components/CommonTable.vue";
+// axios.<method> 能够提供自动完成和参数类型推断功能
+const axios = require("axios").default;
+export default {
+  components: {
+    CommonTable,
+  },
+  name: "onlineexam",
+  data() {
+    return {
+      tableData:'ad'
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      axios
+        .get("https://www.fastmock.site/mock/a809009dfa7a985020b61723d2bb36a1/index/online/OlineExam")
+        // 箭头函数解决vue axios 数据（data）赋值问题
+        .then((tableData)=>{
+          this.tableData = tableData.data.tableData
+          console.log(this.tableData);
+        })
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
