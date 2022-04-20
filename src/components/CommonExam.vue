e96519576e17040b737fee3362f89f23b1f0d045
dgzz <1164549585@qq.com>
2022-04-20 14:57:14 +0800

    试卷左边选择题目样式完成

--- src/components/CommonExam.vue
+++ src/components/CommonExam.vue
@@ -5,40 +5,63 @@
       <el-col :span="24">
 
         <el-card shadow="always">
-          <!-- <div class="exam-title">
-            <div class="l-header"><span>距离考试结束还有: 1小时</span></div>
-            <div class="r-header">
-              <el-button
-                class="submitExam"
-                type="primary"
-                size="small"
-              >交卷</el-button>
-            </div>
-          </div> -->
-
           <span>距离考试结束还有: 1小时</span>
-
           <el-button
             class="submitExam"
             type="primary"
             size="small"
             style="float:right ;margin-top:-5px"
-
           >交卷</el-button>
-
         </el-card>
 
       </el-col>
     </el-row>
     <!-- 一行两列 -->
     <el-row>
-      <el-col :span="12">
-        <div class="grid-content bg-purple">
-          嘻嘻
+      <el-col :span="8">
+        <div class="l-main">
+          <!-- 卡片 -->
+          <el-card
+            class="box-card"
+            shadow="always"
+          >
+            <!-- 单选题 -->
+            <p class="card-title">单选题</p>
+            <div class="tags">
+              <el-tag type="info">1</el-tag>
+              <el-tag type="success">2</el-tag>
+              <el-tag type="info">3</el-tag>
+              <el-tag type="info">4</el-tag>
+              <el-tag type="info">5</el-tag>
+            </div>
+
+            <!-- 多选题 -->
+            <p class="card-title">多选题</p>
+            <div class="tags">
+              <el-tag type="info">6</el-tag>
+              <el-tag type="success">7</el-tag>
+            </div>
+
+            <!-- 判断题 -->
+            <p class="card-title">判断题</p>
+            <div class="tags">
+              <el-tag type="info">8</el-tag>
+              <el-tag type="success">9</el-tag>
+            </div>
+          </el-card>
+
         </div>
       </el-col>
-      <el-col :span="12">
-        <div class="grid-content bg-purple-light">嗯嗯</div>
+      <el-col :span="16">
+        <div class="r-main">
+          <!-- 卡片 -->
+          <el-card
+            class="box-card"
+            shadow="always"
+          >
+            <p>题目</p>
+          </el-card>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -52,16 +75,26 @@ export default {};
 .CommonExam {
   padding: 20px;
 }
-.exam-title {
-  display: flex;
-  height: 20px;
-  flex-direction: row;
-  justify-content: space-between;
-  .r-header {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-self: center;
+.l-main {
+  padding: 20px 20px 20px 0px;
+}
+.r-main {
+  padding: 20px 0px 20px 0px;
+}
+.card-title {
+  background: #eee;
+  line-height: 35px;
+  text-align: center;
+  font-size: 14px;
+}
+
+.tags {
+  padding: 0px 20px 20px 0px;
+  padding-bottom: 0px;
+  // cursor 属性规定要显示的光标的类型（形状）。
+  .el-tag {
+    margin-right: 15px;
+    cursor: pointer;
   }
 }
 </style>
